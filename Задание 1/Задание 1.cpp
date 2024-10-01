74e1c41dc83b6d91f644fb1fe52f735014d68f31
younsiamed <younsiamed@gmail.com>
2024-10-01 19:39:07 +0900

    Lesson-8-2

---
+++ Задание 1/Задание 1.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <algorithm>
+
+class TextComponent {
+public:
+    virtual ~TextComponent() = default;
+    virtual void render(const std::string& text) const = 0;
+};
+
+class Text : public TextComponent {
+public:
+    void render(const std::string& text) const override {
+        std::cout << text;
+    }
+};
+
+class TextDecorator : public TextComponent {
+protected:
+    TextComponent* component;
+public:
+    TextDecorator(TextComponent* component) : component(component) {}
+    virtual ~TextDecorator() {
+        delete component;
+    }
+    void render(const std::string& text) const override {
+        component->render(text);
+    }
+};
+
+class Paragraph : public TextDecorator {
+public:
+    Paragraph(TextComponent* component) : TextDecorator(component) {}
+
+    void render(const std::string& text) const override {
+        std::cout << "<p>";
+        TextDecorator::render(text);
+        std::cout << "</p>";
+    }
+};
+
+class Reversed : public TextDecorator {
+public:
+    Reversed(TextComponent* component) : TextDecorator(component) {}
+
+    void render(const std::string& text) const override {
+        std::string reversed_text = text;
+        std::reverse(reversed_text.begin(), reversed_text.end());
+        TextDecorator::render(reversed_text);
+    }
+};
+
+class Link : public TextDecorator {
+public:
+    Link(TextComponent* component) : TextDecorator(component) {}
+
+    void render(const std::string& url, const std::string& text) const {
+        std::cout << "<a href=\"" << url << "\">";
+        TextDecorator::render(text);
+        std::cout << "</a>";
+    }
+};
+
+int main() {
+    TextComponent* text_block1 = new Paragraph(new Text());
+    text_block1->render("Hello world");
+    std::cout << std::endl;
+
+    TextComponent* text_block2 = new Reversed(new Text());
+    text_block2->render("Hello world");
+    std::cout << std::endl;
+
+    Link* text_block3 = new Link(new Text());
+    text_block3->render("netology.ru", "Hello world");
+    std::cout << std::endl;
+
+    delete text_block1;
+    delete text_block2;
+    delete text_block3;
+
+    return 0;
+}
---
+++ Задание 2/Задание 2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_map>
+
+class VeryHeavyDatabase {
+public:
+    virtual std::string GetData(const std::string& key) {
+        if (key == "key") {
+            return "value";
+        }
+        return "unknown key";
+    }
+
+    virtual ~VeryHeavyDatabase() = default;
+};
+
+class OneShotDB : public VeryHeavyDatabase {
+private:
+    VeryHeavyDatabase* real_object;
+    size_t shots;
+    size_t current_shots;
+
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots = 1)
+        : real_object(real_object), shots(shots), current_shots(0) {}
+
+    std::string GetData(const std::string& key) override {
+        if (current_shots < shots) {
+            ++current_shots;
+            return real_object->GetData(key);
+        }
+        else {
+            return "error";
+        }
+    }
+};
+
+int main() {
+    VeryHeavyDatabase real_db;
+    OneShotDB limit_db(&real_db, 2);
+
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+
+    return 0;
+}
